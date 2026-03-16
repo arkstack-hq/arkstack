1b2b2ffbfdccbeadf68a871bc57fc72237f4fb67
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-16 23:40:10 +0100

    chore: update arkormx and resora versions to 0.2.11 and 0.2.14 respectively, and adjust config paths

--- h3/arkormx.config.js
+++ h3/arkormx.config.js
@@ -1,13 +1,14 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { createArkormCurrentPageResolver } from 'resora'
 import { defineConfig } from 'arkormx'
 
 export default defineConfig({
     paths: {
-        models: './src/models',
-        factories: './database/factories',
-        seeders: './database/seeders',
-        migrations: './database/migrations',
+        models: './src/app/models',
+        factories: './src/database/factories',
+        seeders: './src/database/seeders',
+        migrations: './src/database/migrations',
         buildOutput: './dist',
     },
     outputExt: 'ts',
@@ -17,5 +18,8 @@ export default defineConfig({
                 connectionString: process.env.DATABASE_URL,
             }),
         })
+    },
+    pagination: {
+        resolveCurrentPage: createArkormCurrentPageResolver(),
     },
 })
