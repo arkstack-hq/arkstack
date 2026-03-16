a31b4369c4aa48c83d79c1e1a314a2d76e5404e5
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-16 12:08:31 +0100

    chore: bump version to 0.1.27 and update dependencies to 0.1.25

--- create-arkstack/src/data.ts
+++ create-arkstack/src/data.ts
@@ -25,13 +25,13 @@ export const depsToRemove = [
 ]
 
 export const depsToAdd = {
-    '@arkstack/console-slim': '^0.1.24',
+    '@arkstack/console-slim': '^0.1.25',
 }
 
 export const depsList: Record<string, string> = {
-    '@arkstack/common': '^0.1.24',
-    '@arkstack/console': '^0.1.24',
-    '@arkstack/contract': '^0.1.24',
-    '@arkstack/driver-h3': '^0.1.24',
-    '@arkstack/driver-express': '^0.1.24'
+    '@arkstack/common': '^0.1.25',
+    '@arkstack/console': '^0.1.25',
+    '@arkstack/contract': '^0.1.25',
+    '@arkstack/driver-h3': '^0.1.25',
+    '@arkstack/driver-express': '^0.1.25'
 }
