c69ff0bcaea7f0945f493f7647c4b8d3eec0c093
3m1n3nc3 <mygames.ng@gmail.com>
2026-03-29 09:23:08 +0100

    fix: update package version to 0.3.8 and adjust dependencies in data.ts

--- create-arkstack/src/data.ts
+++ create-arkstack/src/data.ts
@@ -27,14 +27,14 @@ export const depsToRemove = [
 ]
 
 export const depsToAdd = {
-    '@arkstack/console-slim': '^0.3.7',
+    '@arkstack/console-slim': '^0.3.8',
 }
 
 export const depsList: Record<string, string> = {
-    '@arkstack/common': '^0.3.7',
-    '@arkstack/console': '^0.3.7',
-    '@arkstack/contract': '^0.3.7',
-    '@arkstack/driver-h3': '^0.3.7',
-    '@arkstack/filesystem': '^0.3.7',
-    '@arkstack/driver-express': '^0.3.7',
+    '@arkstack/common': '^0.3.8',
+    '@arkstack/console': '^0.3.8',
+    '@arkstack/contract': '^0.3.8',
+    '@arkstack/driver-h3': '^0.3.8',
+    '@arkstack/filesystem': '^0.3.8',
+    '@arkstack/driver-express': '^0.3.8',
 }
