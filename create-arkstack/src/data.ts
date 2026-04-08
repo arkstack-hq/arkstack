6c4d10ab1e89b4b971ccd7be7e30f11840be53fe
3m1n3nc3 <mygames.ng@gmail.com>
2026-04-08 13:59:11 +0100

    fix: update package version to 0.3.11 and dependencies to match

--- create-arkstack/src/data.ts
+++ create-arkstack/src/data.ts
@@ -27,14 +27,14 @@ export const depsToRemove = [
 ]
 
 export const depsToAdd = {
-    '@arkstack/console-slim': '^0.3.10',
+    '@arkstack/console-slim': '^0.3.11',
 }
 
 export const depsList: Record<string, string> = {
-    '@arkstack/common': '^0.3.10',
-    '@arkstack/console': '^0.3.10',
-    '@arkstack/contract': '^0.3.10',
-    '@arkstack/driver-h3': '^0.3.10',
-    '@arkstack/filesystem': '^0.3.10',
-    '@arkstack/driver-express': '^0.3.10',
+    '@arkstack/common': '^0.3.11',
+    '@arkstack/console': '^0.3.11',
+    '@arkstack/contract': '^0.3.11',
+    '@arkstack/driver-h3': '^0.3.11',
+    '@arkstack/filesystem': '^0.3.11',
+    '@arkstack/driver-express': '^0.3.11',
 }
