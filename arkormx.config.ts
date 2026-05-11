5839de41e536f4e10f31ee6ff537a57d2e7cee51
3m1n3nc3 <mygames.ng@gmail.com>
2026-05-11 09:24:51 +0100

    Refactor: Move user-related models to database/models directory
    - Deleted existing models from src/models:
      - PersonalAccessToken
      - User
      - UserNotification
      - UserTwoFactor
    
    - Added new models to database/models with the same structure:
      - PersonalAccessToken
      - User
      - UserNotification
      - UserTwoFactor

--- arkormx.config.ts
+++ arkormx.config.ts
@@ -5,6 +5,7 @@ import { createKyselyAdapter, defineConfig } from 'arkormx'
 
 import { Pool } from 'pg'
 import { createArkormCurrentPageResolver } from 'resora'
+import path from 'node:path'
 
 const db = new Kysely<Record<string, never>>({
     dialect: new PostgresDialect({
@@ -16,10 +17,10 @@ const db = new Kysely<Record<string, never>>({
 
 export default defineConfig({
     paths: {
-        models: './src/models',
-        factories: './database/factories',
-        seeders: './database/seeders',
-        migrations: './database/migrations',
+        models: path.join(process.cwd(), '/database/models'),
+        factories: path.join(process.cwd(), '/database/factories'),
+        seeders: path.join(process.cwd(), '/database/seeders'),
+        migrations: path.join(process.cwd(), '/database/migrations'),
         buildOutput: './dist',
     },
     outputExt: 'ts',
