26b6323be4c7c333300339e2e6b05a22d1f796be
exronnin <neweminence@gmail.com>
2026-05-27 06:16:18 +0100

    refactor: move rawBody and session augmentations to http package.

--- packages/auth/src/app.d.ts
+++ packages/auth/src/app.d.ts
@@ -1,11 +1,9 @@
-import type { User, Auth, Session } from '@arkstack/auth'
+import type { User, Auth } from '@arkstack/auth'
 
 declare module 'node:http' {
   interface IncomingMessage {
     user?: User | undefined;
     auth?: Auth | undefined;
-    rawBody?: Buffer
-    session?: Session | undefined;
     authUser?: User | undefined;
     authToken?: string | undefined;
   }
@@ -15,8 +13,6 @@ declare module 'clear-router/types/h3' {
   interface HttpRequest {
     user?: User | undefined;
     auth?: Auth | undefined;
-    rawBody?: Buffer
-    session?: Session | undefined;
     authUser?: User | undefined;
     authToken?: string | undefined;
   }
@@ -26,8 +22,6 @@ declare module 'clear-router' {
   interface HttpRequests {
     user?: User | undefined;
     auth?: Auth | undefined;
-    rawBody?: Buffer
-    session?: Session | undefined;
     authUser?: User | undefined;
     authToken?: string | undefined;
   }
@@ -37,8 +31,6 @@ declare module 'h3' {
   interface H3EventContext {
     user?: User | undefined;
     auth?: Auth | undefined;
-    rawBody?: Buffer
-    session?: Session | undefined;
     authUser?: User | undefined;
     authToken?: string | undefined;
   }
@@ -49,8 +41,6 @@ declare global {
     interface Request {
       user?: User | undefined;
       auth?: Auth | undefined;
-      rawBody?: Buffer
-      session?: Session | undefined;
       authUser?: User | undefined;
       authToken?: string | undefined;
     }
--- packages/http/src/app.d.ts
+++ packages/http/src/app.d.ts
@@ -1,7 +1,46 @@
-import { ErrorBag } from '.'
+import type { ErrorBag } from '.'
+import type { Session } from '@arkstack/auth'
 
 declare module 'clear-router' {
   interface ClearHttpContext {
     errors: ErrorBag
   }
-} 
+}
+
+
+declare module 'node:http' {
+  interface IncomingMessage {
+    rawBody?: Buffer
+    session?: Session | undefined;
+  }
+}
+
+declare module 'clear-router/types/h3' {
+  interface HttpRequest {
+    rawBody?: Buffer
+    session?: Session | undefined;
+  }
+}
+
+declare module 'clear-router' {
+  interface HttpRequests {
+    rawBody?: Buffer
+    session?: Session | undefined;
+  }
+}
+
+declare module 'h3' {
+  interface H3EventContext {
+    rawBody?: Buffer
+    session?: Session | undefined;
+  }
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      rawBody?: Buffer
+      session?: Session | undefined;
+    }
+  }
+}
