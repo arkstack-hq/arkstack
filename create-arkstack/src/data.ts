9d42186cfe7a8179dd80797f30632aa199248311
exronnin <neweminence@gmail.com>
2026-05-28 13:54:47 +0100

    chore: prepare release v0.11.3

--- create-arkstack/src/data.ts
+++ create-arkstack/src/data.ts
@@ -39,19 +39,19 @@ export const leanDependencies = [
 ]
 
 export const depsList: Record<string, string> = {
-    '@arkstack/auth': '^0.11.2',
-    '@arkstack/common': '^0.11.2',
-    '@arkstack/console': '^0.11.2',
+    '@arkstack/auth': '^0.11.3',
+    '@arkstack/common': '^0.11.3',
+    '@arkstack/console': '^0.11.3',
     '"@arkstack/foundry': '^0.10.10',
-    // '@arkstack/console-slim': '^0.11.2', Depracated
-    '@arkstack/contract': '^0.11.2',
-    '@arkstack/database': '^0.11.2',
-    '@arkstack/driver-express': '^0.11.2',
-    '@arkstack/driver-h3': '^0.11.2',
-    '@arkstack/filesystem': '^0.11.2',
-    '@arkstack/http': '^0.11.2',
-    '@arkstack/view': '^0.11.2',
-    '@arkstack/notifications': '^0.11.2',
+    // '@arkstack/console-slim': '^0.11.3', Depracated
+    '@arkstack/contract': '^0.11.3',
+    '@arkstack/database': '^0.11.3',
+    '@arkstack/driver-express': '^0.11.3',
+    '@arkstack/driver-h3': '^0.11.3',
+    '@arkstack/filesystem': '^0.11.3',
+    '@arkstack/http': '^0.11.3',
+    '@arkstack/view': '^0.11.3',
+    '@arkstack/notifications': '^0.11.3',
 }
 
 export const environment = {
