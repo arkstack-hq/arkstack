d579b35ce285ddde69c59ca46bcb88e250350908
3m1n3nc3 <mygames.ng@gmail.com>
2026-07-14 06:26:03 +0100

    chore: replace swc and manual vitest aliases with vite/vitest native support options.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,26 +1,36 @@
-import { ViteUserConfig, configDefaults, defineConfig } from 'vitest/config'
+import { Plugin, configDefaults, defineConfig } from 'vitest/config'
 
 import path from 'node:path'
 import { pathToFileURL } from 'node:url'
-import swc from 'unplugin-swc'
+
+function appModelsResolver(): Plugin {
+    const modelPattern = /^@app\/models\/([^/]+)$/
+
+    return {
+        name: 'resolve-app-models',
+        enforce: 'pre',
+
+        resolveId(source, importer) {
+            const match = source.match(modelPattern)
+
+            if (!match) {
+                return null
+            }
+
+            const [, model] = match
+            const pkgRoot = (importer ?? '').split('/src/').at(0)!
+
+            return path.resolve(
+                pkgRoot,
+                'src/Contracts',
+                `${model}.ts`,
+            )
+        },
+    }
+}
 
 export default defineConfig({
-
-    plugins: [
-        swc.vite({
-            jsc: {
-                parser: {
-                    syntax: 'typescript',
-                    decorators: true,
-                },
-                transform: {
-                    decoratorMetadata: true,
-                    legacyDecorator: true,
-                },
-                target: 'es2021',
-            },
-        }) as never,
-    ],
+    plugins: [appModelsResolver()],
     resolve: {
         alias: [
             {
@@ -31,18 +41,9 @@ export default defineConfig({
                 find: /^@arkstack\/([^/]+)$/,
                 replacement: path.resolve(__dirname, 'packages') + '/$1/src/index.ts',
             },
-            {
-                find: /^@app\/models\/([^/]+)$/,
-                replacement: '$1',
-                customResolver(source, importer) {
-                    const pkgRoot = (importer ?? '').split('/src/').at(0)!
-
-                    return path.resolve(pkgRoot, 'src/Contracts', `${source}.ts`)
-                },
-            },
         ],
         tsconfigPaths: true,
-    } as ViteUserConfig['resolve'],
+    },
     test: {
         // setup file is at the root of the project, so we need to resolve it not from the current package, but from the root
         setupFiles: [
