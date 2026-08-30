5027c55780413f9aefcbdba8b4446f2ecb765c5b
3m1n3nc3 <mygames.ng@gmail.com>
2026-08-30 08:25:37 +0100

    refactor(limiter): improve rate limiter middleware structure and documentation

--- packages/driver-express/src/middlewares/limiter.ts
+++ packages/driver-express/src/middlewares/limiter.ts
@@ -4,25 +4,33 @@ import { RateLimitExceededException } from '../Exceptions/RateLimitExceededExcep
 import { env } from '@arkstack/common'
 
 /**
- *  create a rate limiter middleware
+ * Create a rate limiter middleware
  * 
- * @param requests   number of requests allowed per windowMs
- * @param perMin     number of minutes for the window
- * @param message    custom message to be returned when rate limit is exceeded
+ * A limiter translates to [Allow N `(requests)` per N seconds `(windowSeconds)` per IP], or
+ * more explicitly `Allow 100 requests per 30 seconds per IP.`
+ * 
+ * @param requests          number of requests allowed per `windowSeconds`
+ * @param windowSeconds     number of seconds for the window
+ * @param message           custom message to be returned when rate limit is exceeded
  * @returns 
  */
 export const limiter = (
     requests: number | ValueDeterminingMiddleware<number> = 100,
-    perSec: number = 900,
+    windowSeconds: number = 900,
     message?: string | ValueDeterminingMiddleware<string>
-) => rateLimit({
-    message,
-    limit: requests, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    windowMs: (env('NODE_ENV') === 'production' ? perSec : 30) * 1000, // 15 minutes
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-    handler: (_, __, ___, options) => {
-        throw new RateLimitExceededException(options)
-    }
-})
+) => {
+    /** 15 minutes default in production */
+    const windowMs = (env('NODE_ENV') === 'production' ? windowSeconds : 30) * 1000
+
+    return rateLimit({
+        message,
+        limit: requests, // Limit each IP to N requests per `window` (here, per 15 minutes)
+        windowMs,
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+        handler: (_, __, ___, options) => {
+            throw new RateLimitExceededException(options)
+        }
+    })
+}
