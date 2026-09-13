1c091229c9691a85969a46d3ffed862b6a906369
3m1n3nc3 <mygames.ng@gmail.com>
2026-09-13 02:45:53 +0100

    fix(middleware): improve type handling and options in FormDataMiddleware

--- packages/driver-express/src/middlewares/formdata.ts
+++ packages/driver-express/src/middlewares/formdata.ts
@@ -8,28 +8,35 @@ export class FormDataMiddleware {
     constructor(type: 'array', fieldName: string, maxCount?: number | undefined, options?: multer.Options)
     constructor(type: 'fields', fields: multer.Field[], options?: multer.Options)
     constructor(type: 'single', fieldName: string, options?: multer.Options)
+    constructor(type: 'none' | 'any', options?: multer.Options)
     constructor(
         private type: 'any' | 'array' | 'fields' | 'none' | 'single',
-        private name?: any,
+        private name?: string | multer.Field[] | multer.Options,
         private count?: any,
         private options?: multer.Options
     ) { }
 
-    handler (req: Request, res: Response, next: NextFunction) {
+    handler(req: Request, res: Response, next: NextFunction) {
         let inst: RequestHandler
         const options: multer.Options = this.options ??
-            (typeof this.count === 'object' && 'storage' in this.count ? this.count : {})
+            (
+                typeof this.name === 'object' && ['any', 'none'].includes(this.type)
+                    ? this.name
+                    : (typeof this.count === 'object' && 'storage' in this.count ? this.count : {})
+            )
 
         const formdata = multer({ storage: multer.memoryStorage(), ...options })
 
         if (this.type === 'any' || this.type === 'none') {
             inst = formdata.any()
         } else if (this.type === 'array') {
-            inst = formdata.array(this.name, this.count)
+            inst = formdata.array(String(this.name), this.count)
         } else if (this.type === 'fields') {
-            inst = formdata.fields(this.name)
+            inst = formdata.fields(
+                Array.isArray(this.name) ? this.name : [this.name as multer.Field]
+            )
         } else {
-            inst = formdata.single(this.name)
+            inst = formdata.single(String(this.name))
         }
 
         return inst.call(inst, req, res, next)
